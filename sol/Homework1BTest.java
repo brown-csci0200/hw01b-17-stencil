04c7becb44931b73cdcb86e5021e70fddfa2c324
Nicholas DeMarinis <ndemarinis@wpi.edu>
2023-01-31 01:00:52 -0500

    Initial commit.

---
+++ sol/Homework1BTest.java
@@ -0,0 +1,11 @@
+package sol;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class Homework1BTest {
+}
